9849411422fb38e06472f21fb40c1793caaacf3e
Artem <macromicro37@gmail.com>
2021-06-23 17:30:32 +0300

    test 1

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,13 +1,23 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
     @Autowired
+    @Qualifier("dog")
     private Animal animal;
+
+
+    @Bean(name="timer")
+    public Timer getTimer(){
+        Timer timer = new Timer();
+        return timer;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
